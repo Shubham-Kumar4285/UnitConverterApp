5a4e86eaa16b6c16687339cffa3e459a70a19288
Shubham_Kumar <shubham124285@gmail.com>
2024-03-04 13:44:01 +0530

    Test commit

---
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.unitconverterapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UnitConverterAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    UnitConverter()
+                }
+            }
+        }
+    }
+}
+//rows and columns
+@Composable
+fun UnitConverter(){
+        Column {
+            Text(text = "Unit Converter")
+            OutlinedTextField(value = "", onValueChange = {
+            //code to execute when value changes
+            })
+            Row {
+                val context= LocalContext.current;
+
+                Button(onClick = {
+                    Toast.makeText(context,"You clicked me !",Toast.LENGTH_SHORT).show()
+                })
+                {
+                    Text(text = "Press Here !")
+                }
+            }
+
+            Text(text = "Result ")
+
+        }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UnitConverterPreview(){
+    UnitConverter()
+}
+}
